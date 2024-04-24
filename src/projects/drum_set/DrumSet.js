eb07922d72258864978b3333c43b14e0ad4c7d1c
ashish <ashish.2octomber@gmail.com>
2024-04-24 15:03:45 +0530

    responsive

--- src/projects/drum_set/DrumSet.js
+++ src/projects/drum_set/DrumSet.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import drum1 from '../../assets/images/drums/crash.png';
 import drum2 from '../../assets/images/drums/kick.png';
 import drum3 from '../../assets/images/drums/snare.png';
@@ -16,123 +16,32 @@ import sound6 from '../../assets/sounds/tom-3.mp3';
 import sound7 from '../../assets/sounds/tom-4.mp3';
 
 const DrumSet = () => {
-
     const music = [
-        {
-            picture: drum1,
-            sound: sound1,
-            key: "a"
-        },
-        {
-            picture: drum2,
-            sound: sound2,
-            key: "s"
-
-        },
-        {
-            picture: drum3,
-            sound: sound3,
-            key: "d"
-        },
-        {
-            picture: drum4,
-            sound: sound4,
-            key: "f"
-        },
-        {
-            picture: drum5,
-            sound: sound5,
-            key: "g"
-        },
-        {
-            picture: drum6,
-            sound: sound6,
-            key: "h"
-        },
-        {
-            picture: drum7,
-            sound: sound7,
-            key: "j"
-        },
+        { picture: drum1, sound: sound1, key: "a" },
+        { picture: drum2, sound: sound2, key: "s" },
+        { picture: drum3, sound: sound3, key: "d" },
+        { picture: drum4, sound: sound4, key: "f" },
+        { picture: drum5, sound: sound5, key: "g" },
+        { picture: drum6, sound: sound6, key: "h" },
+        { picture: drum7, sound: sound7, key: "j" },
     ];
-   const [modi, setModi]=useState(true)
-    // useEffect(() => {
-    //     const handleKeyDown = (e) => {
-    //         console.log(e.key);
-    //         playDrum(e.key);
-    //     };
-
-    //     document.addEventListener('keydown', handleKeyDown);
-
-    //     // Remove the event listener when component unmounts
-    //     return () => {
-    //         document.removeEventListener('keydown', handleKeyDown);
-    //     };
-    // }, [modi]);
-       
- 
 
     const playDrum = (key) => {
-        console.log(key)
-        switch (key) {
-            case "a": {
-                const newsound = new Audio(sound1);
-                newsound.play();
-            }
-                break;
-            case "s": {
-                const newsound = new Audio(sound2);
-                newsound.play();
-            }
-                break;
-            case "d": {
-                const newsound = new Audio(sound3);
-                newsound.play();
-            }
-                break;
-            case "f": {
-                const newsound = new Audio(sound4);
-                newsound.play();
-            }
-                break;
-            case "g": {
-                const newsound = new Audio(sound5);
-                newsound.play();
-            }
-                break;
-            case "h": {
-                const newsound = new Audio(sound6);
-                newsound.play();
-            }
-                break;
-            case "j": {
-                const newsound = new Audio(sound7);
-                newsound.play();
-            }
-                break;
-
-            default: ;
-           
-        }
-
+        const sound = new Audio(music.find(item => item.key === key).sound);
+        sound.play();
     }
 
     return (
-        <div className='center h-screen '>
-            <div className="flex gap-5">
-                {music.map((item, index) => {
-                    return (
-                        <div className="border-2 border-blue-200 shadow-2xl rounded-full center p-10 cursor-pointer " key={index}
-                            onClick={() => playDrum( item.key)}
-                        >
-                            <img src={item.picture} alt="drum" className='size-40 ' />
-                            <div className=""  tabIndex={0} onKeyDown={()=>alert("key pressed")}></div>
-                        </div>
-                    )
-                })}
+        <div className="flex justify-center items-center h-screen">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                {music.map((item, index) => (
+                    <div key={index} className="cursor-pointer" onClick={() => playDrum(item.key)}>
+                        <img src={item.picture} alt="drum" className="w-24 md:w-32 lg:w-40 xl:w-48 mx-auto" />
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default DrumSet
+export default DrumSet;
